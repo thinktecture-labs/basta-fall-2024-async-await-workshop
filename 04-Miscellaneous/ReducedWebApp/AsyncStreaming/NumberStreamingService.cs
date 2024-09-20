e67ae876554a5725218c789cdefbc6a45a286bdf
Kenny Pflug <kenny.pflug@thinktecture.com>
2024-09-20 19:12:39 +0200

    feat: prepare hands-on 5
    Signed-off-by: Kenny Pflug <kenny.pflug@thinktecture.com>

--- 04-Miscellaneous/ReducedWebApp/AsyncStreaming/NumberStreamingService.cs
+++ 04-Miscellaneous/ReducedWebApp/AsyncStreaming/NumberStreamingService.cs
@@ -1,10 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 using ProtoBuf.Grpc;
-using ReducedWebApp.AsyncStreaming;
+using WebApi.AsyncStreaming;
 
-namespace WebApi.AsyncStreaming;
+namespace ReducedWebApp.AsyncStreaming;
 
 public sealed class NumberStreamingService : INumberStreamingService
 {
@@ -15,14 +13,44 @@ public sealed class NumberStreamingService : INumberStreamingService
     public IAsyncEnumerable<StreamNumbersResponseDto> StreamNumbers(
         StreamNumbersRequestDto dto,
         CallContext context = default
-    )
-    {
-        throw new NotImplementedException();
-    }
+    ) =>
+        dto.CancelAllStreams ?
+            StreamForAdminCallerAsync(context.CancellationToken) :
+            StreamForStandardCallerAsync(context.CancellationToken);
 
     public Task<StreamingResponseDto> StartStreaming(CallContext callContext = default)
     {
         var result = _numberGenerator.TryStartGeneratingNumbers();
         return Task.FromResult(new StreamingResponseDto { IsSuccess = result });
+    }
+
+    // Not necessary because we register explicitly on the cancellation token
+#pragma warning disable CS8425 // Async-iterator member has one or more parameters of type 'CancellationToken' but none of them is decorated with the 'EnumeratorCancellation' attribute, so the cancellation token parameter from the generated 'IAsyncEnumerable<>.GetAsyncEnumerator' will be unconsumed
+    private async IAsyncEnumerable<StreamNumbersResponseDto> StreamForAdminCallerAsync(
+        CancellationToken cancellationToken
+    )
+    {
+        await using var registration = cancellationToken.Register(
+            state => ((NumberGenerator) state!).TryStopGeneratingNumbers(),
+            _numberGenerator
+        );
+
+        // ReSharper disable once UseCancellationTokenForIAsyncEnumerable -- already handled by the registration
+        await foreach (var number in _numberGenerator.GetAsyncEnumerable())
+        {
+            yield return new StreamNumbersResponseDto { Number = number };
+        }
+    }
+#pragma warning restore CS8425
+
+
+    private async IAsyncEnumerable<StreamNumbersResponseDto> StreamForStandardCallerAsync(
+        [EnumeratorCancellation] CancellationToken cancellationToken
+    )
+    {
+        await foreach (var number in _numberGenerator.GetAsyncEnumerable().WithCancellation(cancellationToken))
+        {
+            yield return StreamNumbersResponseDto.FromNumber(number);
+        }
     }
 }
